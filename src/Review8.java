bee1b7c03375a202c55171ef3791da08c34faeed
Koki Nakata <241033@std.hi-joho.ac.jp>
2025-05-16 12:02:48 +0900

    java課題8

---
+++ src/Review8.java
@@ -0,0 +1,11 @@
+public class Review8 {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+        b = a;
+        a = 10000;
+        b = (int)a;
+        float f = 5.94f;
+        short s = 10;
+    }
+}
