fd9b374de2f1b9db12848d70e46eb5a42a00ffad
Koki Nakata <241033@std.hi-joho.ac.jp>
2025-05-19 12:04:34 +0900

    java課題21

--- src/Review21.java
+++ src/Review21.java
@@ -4,13 +4,13 @@ public class Review21 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
-        int count = num;
         int sum = 0;
-        while (count > 0) {
-            if(num % 2 == 0) {
-                sum += num;
+        int i = 1;
+        while (i <= num) {
+            if(i % 2 == 0) {
+                sum += i;
             }
-            num--;
+            i++;
         }
         System.out.println(sum);
     }
