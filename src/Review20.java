718d82bc37631dfcd261f08f59cb8a327c8a2c03
Koki Nakata <241033@std.hi-joho.ac.jp>
2025-05-19 12:06:28 +0900

    java課題20

--- src/Review20.java
+++ src/Review20.java
@@ -4,10 +4,10 @@ public class Review20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int reNum = num;
-        while (reNum > 1) {
-            num += (num -1);
-            num--;
+        int i = 1;
+        int sum  = 0;
+        while (i <= num) {
+            sum += i;
         }
         System.out.println(num);
     }
