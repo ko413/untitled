c1442369f5b5db1b8f931e4c7229932d78945481
Koki Nakata <241033@std.hi-joho.ac.jp>
2025-05-19 11:44:41 +0900

    java課題17

---
+++ src/Review17.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Review17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        for (i = 1; i <= num; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
