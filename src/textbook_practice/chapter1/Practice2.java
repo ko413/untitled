ed8a45565934a8e7811652e8c9afff34dfc6600b
Koki Nakata <241033@std.hi-joho.ac.jp>
2025-05-26 11:07:26 +0900

    javaテキスト課題1-2

---
+++ src/textbook_practice/chapter1/Practice2.java
@@ -0,0 +1,9 @@
+package textbook_practice.chapter1;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println("縦幅" + a + "横幅" + b +"の長方形の面積は、" + a * b);
+    }
+}
